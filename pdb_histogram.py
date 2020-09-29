180dbbafed9fc9ef95b449de07bd9d50af8248ea
bendorr2415 <bendorr2415@gmail.com>
2020-09-29 13:30:30 -0700

    Made plot_hist function. Passed in data dictionary and made a pandas df in the function.

--- pdb_histogram.py
+++ pdb_histogram.py
@@ -76,7 +76,7 @@ def sort_dict(dict):
 def plot_hist(data):
     df = pd.DataFrame(data)
     df.plot(x='Dates',y='Weights',kind='bar',color='red')
-    ax=data['Number of Structures'].plot(secondary_y=True,color='blue',kind='bar')
+    ax=df['Number of Structures'].plot(secondary_y=True,color='blue',kind='bar')
     ax.set_ylabel('num of struc')
     plt.show()
 
